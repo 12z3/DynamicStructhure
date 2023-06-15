e965dc02ec935a3d98c3340c5ad57d76c723ffec
12z3 <77529497+12z3@users.noreply.github.com>
2023-06-15 10:56:34 +0300

    Add files via upload

--- bsPeakArray.java
+++ bsPeakArray.java
@@ -1,9 +1,6 @@
 package dynamicsStructhure;
 
 public class bsPeakArray {
-    // https://leetcode.com/problems/peak-index-in-a-mountain-array/
-    // https://leetcode.com/problems/find-in-mountain-array/description/
-
     public static void main(String[] args) {
         //int[] x1 = {-4, -3, -2, -1, 0, 1, 5, 6, 7, 8, 5, 4, 3, 2, 1};
         //           0   1   2   3  4  5  6  7  8  9  10 11 12 13 14
