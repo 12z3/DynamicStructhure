3c857df87bac9098252e40c8886d371b436732e8
12z3 <77529497+12z3@users.noreply.github.com>
2023-06-23 06:57:22 +0300

    Add files via upload

--- bs.java
+++ bs.java
@@ -5,11 +5,11 @@ import java.util.Arrays;
 public class bs {
     //https://leetcode.com/problems/search-in-rotated-sorted-array/
     public static void main(String[] args) {
-        int target = 4;
+        int target = 1;
         //int[] a = {0, 1, 2, 4, 5, 6, 7};
         //     idx   0  1  2  3  4  5  6
-        int[] a = {4, 5, 6, 7, 0, 1, 2}; // 0
-        //int[] a = {1, 3}; // 1
+        //int[] a = {4, 5, 6, 7, 0, 1, 2}; // 0
+        int[] a = {1, 3}; // 1
         //int[] a = {1, 3, 5,}; // 1
         //int[] a = {4, 5, 6, 7, 0, 1, 2}; // 0
 
