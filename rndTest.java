0cf89cdf3eb3082fd8879b9c75a622feca1822b8
12z3 <77529497+12z3@users.noreply.github.com>
2023-07-11 13:14:22 +0300

    Add files via upload

--- rndTest.java
+++ rndTest.java
@@ -6,6 +6,8 @@ public class rndTest {
     public static void main(String[] args) {
         int[] x = {1, 2, 3, 4, 5, 6, 7};  // 7 - 2 = 5
         int[] a = {1, 1, 1, 3, 3, 3, 3, 7, 7};  // 7 - 2 = 5
+        int[] y = {1, 0, 3, 2, 0};
+        int[] t = {13, 46, 24, 52, 20, 9};
         System.out.println(Arrays.toString(x));
         System.out.println(Arrays.toString(rotatedArr(x, 3)));
         //System.out.println(Arrays.toString(rotatedArr1(x, 2)));
@@ -21,6 +23,9 @@ public class rndTest {
         int[] arr = {2, 2, 1, 1, 1, 2, 2};
         int ans = majorityElement(arr);
         System.out.println("The majority element is: " + ans);
+        sumProblem(y, 3);
+        selection_sort(t);
+
 
     }
 
@@ -56,7 +61,7 @@ public class rndTest {
     }
 
     static Map<Integer, Integer> countDigitsMatch(int[] a) {
-        Map<Integer, Integer> digitsCount = new HashMap<>();
+        Map<Integer, Integer> digitsCount = new LinkedHashMap<>();
 
         for (int j : a) digitsCount.put(j, 0);
         for (int j : a) {
@@ -93,30 +98,76 @@ public class rndTest {
         return countDigits;
     }
 
-    // https://takeuforward.org/data-structure/find-the-majority-element-that-occurs-more-than-n-2-times/
-        public static int majorityElement(int []v) {            // 2, 2, 1, 1, 1, 2, 2
-            //size of the given array:
-            int n = v.length;
-            int cnt = 0; // count
-            int el = 0; // Element
+    // https://takeuforward.org/data-structure/two-sum-check-if-a-pair-with-given-sum-exists-in-array/
+    static void sumProblem(int[] a, int target) {            // 2, 3, 1, 4    t = 4
+        Map<Integer, Integer> tmp = new HashMap<>();
+        Map<Integer, Integer> res = new HashMap<>();
 
-            //applying the algorithm:
-            for (int i = 0; i < n; i++) {
-                if (cnt == 0) {
-                    cnt = 1;
-                    el = v[i];
-                } else if (el == v[i]) cnt++;
-                else cnt--;
+
+        for (int i = 0; i < a.length; i++) tmp.put(i, a[i]);
+        for (int i = 0; i < a.length; i++) {
+            int selected = tmp.get(i);
+            int searchingEl = (target - selected);
+            tmp.remove(i);
+            if (tmp.containsValue(searchingEl)) {
+                res.put(a[i], searchingEl);
             }
-
-            //checking if the stored element
-            // is the majority element:
-            int cnt1 = 0;
-            for (int i = 0; i < n; i++) {
-                if (v[i] == el) cnt1++;
-            }
-
-            if (cnt1 > (n / 2)) return el;
-            return -1;
         }
+
+        for (Map.Entry<Integer, Integer> el : res.entrySet())
+            System.out.print(el.getKey() + " -> " + el.getValue() + "; ");
+
+    }
+
+    // https://takeuforward.org/data-structure/find-the-majority-element-that-occurs-more-than-n-2-times/
+    public static int majorityElement(int[] v) {            // 2, 2, 1, 1, 1, 2, 2
+        //size of the given array:
+        int n = v.length;
+        int cnt = 0; // count
+        int el = 0; // Element
+
+        //applying the algorithm:
+        for (int i = 0; i < n; i++) {
+            if (cnt == 0) {
+                cnt = 1;
+                el = v[i];
+            } else if (el == v[i]) cnt++;
+            else cnt--;
+        }
+
+        //checking if the stored element
+        // is the majority element:
+        int cnt1 = 0;
+        for (int i = 0; i < n; i++) {
+            if (v[i] == el) cnt1++;
+        }
+
+        if (cnt1 > (n / 2)) return el;
+        return -1;
+    }
+
+    // https://takeuforward.org/sorting/selection-sort-algorithm/
+    // Взима 1-ят ел и го Обявява за мин. Сравнява го със всички останали. Ако е най-малкият си остава на мястото.
+    // Ако ли Не то разменя местата на този ел с най-малкият от всички останали елементи.
+    static void selection_sort(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int mini = i;                             // <-  !!!
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[mini]) {
+                    mini = j;
+                }
+            }
+            //swap
+            int temp = arr[mini];
+            arr[mini] = arr[i];
+            arr[i] = temp;
+        }
+
+        System.out.println("After selection sort:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
 }
