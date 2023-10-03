e0b1ef85f9a1cf8ec99e99cec6cdbd95a72c5bff
12z3 <77529497+12z3@users.noreply.github.com>
2023-10-03 19:13:59 +0300

    Add files via upload

--- DynamicArray.java
+++ DynamicArray.java
@@ -1,45 +1,49 @@
-package DynamicsStructhure;
+package dynamicsStructhure;
 
 import training.Methods;
+
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class DynamicArray extends Methods {
 
     // https://softuni.bg/trainings/resources/video/62383/video-02-july-2021-georgi-georgiev-data-structures-fundamentals-with-java-june-2021/3417
 
+    // КЪДЕ Е ГРЕШКАТА: removeElementFromArray -> [1, 2, 5, 4, 67, 9, 0], "3"  Липсва ;) ?
     /**
      * Проверява дали индекса е валиден.
+     *
      * @param arr -> Проверявания масив
      * @return -> резултата от проверката.
      */
 
-    public static boolean checkPosition(int[] arr) {
+    public static boolean checkPosition(int[] arr, int index) {
         int size = arr.length;
         boolean isNoValidIndex = false;
         try {
-            if (size > arr.length) {
+            if (index > size) {
                 isNoValidIndex = true;
                 throw new ArrayIndexOutOfBoundsException();
             }
-        } catch (ArrayIndexOutOfBoundsException ex){
+        } catch (ArrayIndexOutOfBoundsException ex) {
             System.out.print("Index Out Of Bounds");
         }
-      return isNoValidIndex;
+        return isNoValidIndex;
     }
 
-    public static void addElementIntoArray(int newElement, int[] arr, int index) {
+    public static void addElementIntoArrayS(int newElement, int[] arr, int index) {
         int size = arr.length;
         int countZeroElement = 0;
 
-        if (checkPosition(arr)){
-            System.out.println("Index is OUT Of BOUNDS!");
-            return;
-        }
-            for (int i = 0; i < size; i++) {
-                if (arr[i] == 0) {
-                    countZeroElement++;
-                }
+//        if (checkPosition(arr, index)) {
+//            System.out.println("Index is OUT Of BOUNDS!");
+//            return;
+//        }
+        for (int i = 0; i < size; i++) {
+            if (arr[i] == 0) {
+                countZeroElement++;
             }
+        }
 
         if (countZeroElement <= 3) {
             int[] temp = new int[size + size / 2];
@@ -48,7 +52,7 @@ public class DynamicArray extends Methods {
             }
             arr = temp;                                  // Изчиства от памета старият масив "arr"  <-- !!!
         }
-                                                         // Отзад напред до "index" включително <--
+        // Отзад напред до "index" включително <--
         for (int i = size; i >= index; i--) {   // Премества елементи на ДЯСНО, започвайки от от "size" с една позиция.
             arr[i] = arr[i - 1];                         // Последният става = на предпоследният и т.н. до index вкл.
         }                                                // "i = size" това налага size = arr.length - 1;
@@ -60,23 +64,24 @@ public class DynamicArray extends Methods {
         }
 
         printArrayWithOutZeroElements(arr);
+        //return arr;
     }
 
     public static void removeElementFromArray(int index, int[] arr) {
         int size = arr.length;
 
-        if (checkPosition(arr)){
+        if (checkPosition(arr, index)) {
             System.out.println("Index is OUT Of BOUNDS!");
             return;
         }
         int removedEl = arr[index];
-                                                        // От следващият след "index" елем. до последният.
-        for (int i = index + 1; i < arr.length; i++) {  // Тъй като е "arr[i - 1] = arr[i]" то трябва "i = index + 1".
-            arr[i - 1] = arr[i];             // Премества елементи на ЛЯВО, Започвайки от "index + 1" с една позиция.
-        }                                    //arr[index + 1 - 1] = arr[index];
-        // size--;
-       // arr[arr.length - 1] = 0;
-        printArrayWithOutZeroElements(arr);
+
+        for (int i = index; i < size - 1; i++) {
+            arr[i] = arr[i + 1];             // Премества елементи на ЛЯВО, Започвайки от "index" с една позиция.
+        }                                    // От следващият след "index" елем. до последният. ???
+        arr[arr.length - 1] = 0;
+        //printArrayWithOutZeroElements(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
     public static void main(String[] args) {
@@ -87,8 +92,9 @@ public class DynamicArray extends Methods {
         int[] a = {1, 2, 5, 3, 4, 67, 9};
 
         printIntArray(a);
-        addElementIntoArray(333, a, 3);
-        removeElementFromArray(3,a);
+        addElementIntoArrayS(333, a, 3);
+        //removeElementFromArray(3, addElementIntoArrayS(333, a, 3));
+        removeElementFromArray(3, a);
 
     }
 }
