e45d5a74b73809526e0a54ee25b773bb64b0f60e
12z3 <77529497+12z3@users.noreply.github.com>
2023-12-19 06:30:12 +0200

    Add files via upload

--- toto.java
+++ toto.java
@@ -5,39 +5,64 @@ import java.util.*;
 public class toto {
     public static void main(String[] args) {
         List<Integer> officialResult = new ArrayList<>(List.of(1, 7, 11, 33, 48, 49));
-        System.out.println();
-        int attempts = 5;
-
-        play(officialResult, attempts);
+        int matches = 5;
+        play(officialResult, matches);
     }
 
-    private static void play(List<Integer> officialResult, int attempts) {
+    private static void play(List<Integer> officialResult, int matches) {
+        int attempts = 0;
+
+        System.out.println();
         System.out.println("Lottery draw:   \n" + officialResult);
         System.out.println("\nYour Choices:");
         List<List<Integer>> result = generateRandomLists(8, 6);
 
         printResult(result);
         System.out.println("\nMatches:");
-        // checkMatches(result, officialResult, 3);
-
-        int cnt = 0;
-        int tmp = checkMatches(result, officialResult, attempts);
-        while (!(tmp == attempts)) {
-            tmp = checkMatches(generateRandomLists(8, 6), officialResult, attempts);
-            cnt++;
-            System.out.println("Total numbers of attempts: " + cnt * 16);
-            System.out.println();
+        int tmp = checkMatches(result, officialResult, matches);
+        while (!(tmp == matches)) {
+            //printResult(result);
+            tmp = checkMatches(generateRandomLists(8, 6), officialResult, matches);
+            attempts++;
         }
+        System.out.println("\nAll available attempts are: " + attempts + " for " + matches + " digits");
     }
 
-    private static int checkMatches(List<List<Integer>> res, List<Integer> arr, int x) {
-        int tmp = 0;
-        for (List<Integer> resX : res) {
-            tmp = checkResults(arr, resX);
-            if (tmp == x) return x;
+    private static List<List<Integer>> generateRandomLists(int choices, int counting) {
+        List<List<Integer>> resList = new ArrayList<>();
+        Set<Integer> addedDigit = new HashSet<>();
+        List<Integer> result = null;
+
+        Random rnd = new Random();
+        for (int i = 0; i < choices; i++) {
+            Set<Integer> tmp = new HashSet<>();
+            for (int j = 0; j < counting; j++) {
+                int newEl = rnd.nextInt(1, 50);
+                if (!addedDigit.contains(newEl) && !tmp.contains(newEl)) {
+                    tmp.add(newEl);
+                    addedDigit.add(newEl);                   // Помни всички генерирани до тук числа.
+                } else {
+                    j--;
+                }
+            }
+            result = new ArrayList<>(tmp);
+            Collections.sort(result);
+            resList.add(result);
         }
-        //System.out.println();
-        return tmp;
+        return resList;
+    }
+
+    private static int checkMatches(List<List<Integer>> res, List<Integer> officialResult, int matches) {
+        for (List<Integer> item : res) {
+            int tmp = checkResults(officialResult, item);
+            if (tmp == matches) {
+                System.out.println();
+                printResult(res);
+                return tmp;
+            }
+        }
+        System.out.println();
+        return -1;
     }
 
     private static int checkResults(List<Integer> a, List<Integer> b) {
@@ -52,36 +77,12 @@ public class toto {
                 }
             }
         }
-        System.out.println("cnt = " + cnt + ": " + tmp);
+        System.out.println("matches = " + cnt + ": " + tmp);
         return cnt;
     }
 
     private static void printResult(List<List<Integer>> res) {
         int idx = 1;
         for (List<Integer> re : res) System.out.println(idx++ + ": " + re);
-    }
-
-    private static List<List<Integer>> generateRandomLists(int choices, int counting) {
-        List<List<Integer>> resList = new ArrayList<>();
-        Set<Integer> addedDigit = new HashSet<>();
-        List<Integer> result = null;
-
-        Random rnd = new Random();
-        for (int i = 0; i < choices; i++) {
-            Set<Integer> tmp = new HashSet<>();
-            for (int j = 0; j < counting; j++) {
-                int newEl = rnd.nextInt(50);
-                if (newEl != 0 && !addedDigit.contains(newEl) && !tmp.contains(newEl)) {
-                    tmp.add(newEl);
-                    addedDigit.add(newEl);                   // Memory на генерираните до тук чисълца.
-                } else {
-                    j--;
-                }
-            }
-            result = new ArrayList<>(tmp);
-            Collections.sort(result);
-            resList.add(result);
-        }
-        return resList;
     }
 }
