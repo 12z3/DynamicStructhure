f6148d21393214fc37e888450b381001779e935c
12z3 <77529497+12z3@users.noreply.github.com>
2023-12-27 20:57:02 +0200

    Add files via upload

---
+++ Jackpot.java
@@ -0,0 +1,207 @@
+package dynamicsStructure;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class Jackpot {
+    public static void main(String[] args) throws IOException {
+        int choices = 3, counting = 6, idx = 1;
+
+        List<Integer> officialResult = Arrays.asList(5, 9, 23, 28, 34, 43);
+        System.out.println("Последно теглене: " + officialResult + "\n");
+
+        List<List<Integer>> newDraw = generateRandomResults(choices, counting, officialResult);
+        for (List<Integer> item : newDraw) System.out.println((idx++) + ". " + item);
+
+        String check = (checkResults(newDraw, officialResult) == 1) ? "Дублира се. " : "Няма дублирани елементи.";
+        System.out.println(check + "\n");
+        saveInFile(newDraw, officialResult);
+    }
+
+    private static List<List<Integer>> generateRandomResults(int choices, int counting, List<Integer> official) {
+        Scanner scanner = new Scanner(System.in);
+        Random rnd = new Random();
+
+        List<List<Integer>> resList = new ArrayList<>();
+        Set<Integer> addedDigit = new HashSet<>();
+        List<Integer> result;
+
+        System.out.print("Да се отчита ли предишното теглене?  (Y / N): ");
+        String answer = scanner.nextLine();
+        boolean draw = validatedAnswers(answer).equalsIgnoreCase("Y");
+
+        for (int i = 0; i < choices; i++) {
+            Set<Integer> tmp = new HashSet<>();
+            for (int j = 0; j < counting; j++) {
+                int newEl = rnd.nextInt(1, 50);
+                if (draw) {
+                    if (!addedDigit.contains(newEl) && !tmp.contains(newEl)) {
+                        tmp.add(newEl);
+                        addedDigit.add(newEl);
+                    } else {
+                        j--;
+                    }
+                } else {
+                    if (!addedDigit.contains(newEl) && !tmp.contains(newEl) && !official.contains(newEl)) {
+                        tmp.add(newEl);
+                        addedDigit.add(newEl);
+                    } else {
+                        j--;
+                    }
+                }
+            }
+            result = new ArrayList<>(tmp);
+            Collections.sort(result);
+            resList.add(result);
+        }
+        return resList;
+    }
+
+    private static void saveInFile(List<List<Integer>> res, List<Integer> official) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        File file = new File("tResN.txt");
+        boolean doDelete;
+
+        System.out.print("Да се запазят ли предишните резултати (Y / N) или (E) за изход: ");
+        String answer = scanner.nextLine();
+
+        if (answer.equalsIgnoreCase("E")) {
+            return;  // последен метод е - 23 ред.
+        } else doDelete = validatedAnswers(answer).equalsIgnoreCase("Y");
+
+        //doDelete = validatedAnswers(answer).equalsIgnoreCase("Y") ? true : false;
+        writer(res, official, file, doDelete);
+    }
+
+    private static void writer(List<List<Integer>> res, List<Integer> official, File file, boolean doDelete)
+            throws IOException {
+
+        FileWriter writer = new FileWriter(file, doDelete);
+
+        writer.write("\n" + "Последно тегелене: " + official.toString() + "\n");
+        writer.write("---------------------------" + "\n");
+        writer.write("Предложения: " + "\n");
+        for (int i = 0; i < res.size(); i++) {
+            writer.write((i + 1) + ". " + res.get(i).toString() + "\n");
+        }
+        System.out.println("Резултата е записан в: " + file.getAbsolutePath());
+        writer.write("---------------------------" + "\n");
+        writer.write(getDateAndTime() + "\n");
+        writer.write(dateFromNextDraw() + "\n");
+        writer.close();
+    }
+
+    private static String validatedAnswers(String answer) {
+        Scanner scanner = new Scanner(System.in);
+        while (!answer.equalsIgnoreCase("Y") && !answer.equalsIgnoreCase("N")) {
+            System.out.print("Y / N: ");
+            answer = scanner.nextLine();
+        }
+        //scanner.close();;
+        return answer;
+    }
+
+    private static String getDateAndTime() {
+        DateTimeFormatter formatDate =
+                DateTimeFormatter.ofPattern("dd MMM yyyy, E - a- c 'ден:' HH:mm:ss ч ");
+
+        LocalDateTime now = LocalDateTime.now();
+        return now.format(formatDate);
+    }
+
+    private static String dateFromNextDraw() {
+        LocalDateTime now = LocalDateTime.now();
+
+        Integer[] drawDays = {3, 8};
+        String[] days = {"MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY", "SUNDAY"};
+        String thisDayStr = String.valueOf(now.getDayOfWeek());
+        String drawDay = "";
+        String printDay;
+        String resStr;
+        int thisDay = -1;
+        int leftDays = -1;
+
+        //long daysDifference = ChronoUnit.DAYS.between();
+        //System.out.println(String.valueOf(now.getDayOfWeek()));
+
+        for (int i = 0; i < days.length; i++) {
+            if (days[i].equalsIgnoreCase(thisDayStr)) thisDay = i;
+        }
+
+        // TODO: Много е съмнителна логиката.
+        if (thisDay == drawDays[0]) {
+            leftDays = 0;
+            drawDay = "Четвъртък";
+        } else if (thisDay < drawDays[0]) {
+            leftDays = thisDay - drawDays[0];
+            drawDay = "Четвъртък";
+        } else if (thisDay > drawDays[0] && thisDay < drawDays[1]) {
+            leftDays = thisDay - drawDays[1];
+            drawDay = "Неделя";
+        } else if (thisDay == drawDays[1]) {
+            leftDays = 0;
+            drawDay = "Неделя";
+        }
+
+        thisDayStr = getCurrentDay(thisDayStr);
+
+        printDay = (Math.abs(leftDays) == 1) ? " ден." : " дни.";
+
+        String resStrA = ("Днес е " + thisDayStr + ", следващият тираж е в " + drawDay +
+                ". Остават " + Math.abs(leftDays) + printDay);
+
+        String resStrB = ("Днес е " + thisDayStr + ", следващият тираж е в " + drawDay +
+                ". Тиража е днес ");
+
+        resStr = (leftDays == 0) ? resStrB : resStrA;
+        return resStr;
+    }
+
+    private static String getCurrentDay(String thisDayStr) {
+        switch (thisDayStr) {
+            case "MONDAY" -> thisDayStr = "Понеделник";
+            case "TUESDAY" -> thisDayStr = "Вторник";
+            case "WEDNESDAY" -> thisDayStr = "Сряда";
+            case "THURSDAY" -> thisDayStr = "Четвъртък";
+            case "FRIDAY" -> thisDayStr = "Петък";
+            case "SATURDAY" -> thisDayStr = "Събота";
+            case "SUNDAY" -> thisDayStr = "Неделя";
+        }
+        return thisDayStr;
+    }
+
+    private static int checkResults(List<List<Integer>> res, List<Integer> official) {
+        Set<Integer> officialSet = new HashSet<>(official);
+        List<Integer> matches;
+        int isMatch = -1;
+        for (int i = 0; i < res.size(); i++) {
+            for (int j = 0; j < res.get(i).size(); j++) {
+                matches = new ArrayList<>();
+                if (officialSet.contains(res.get(i).get(j))) {
+                    matches.add(res.get(i).get(j));
+                    System.out.println(("Ред: " + (i + 1)) + " Колона: " + j + " - " + matches + " ");
+                    isMatch = 1;
+                }
+            }
+        }
+        return isMatch;
+    }
+
+    private static int checkResultsOld(List<List<Integer>> res, List<Integer> official) {
+        for (List<Integer> item : res) {
+            for (int i = 0; i < item.size(); i++) {
+                for (int j = 0; j < official.size(); j++) {
+                    if (item.get(i).equals(official.get(j))) {
+                        System.out.printf("ERROR :( -> %d ", official.get(j));
+                        return 1;
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+}
