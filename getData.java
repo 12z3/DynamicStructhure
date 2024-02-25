a79f2cd9481a1a86028b8b419e0fd45457c3b01d
12z3 <77529497+12z3@users.noreply.github.com>
2024-02-25 18:02:42 +0200

    Add files via upload

--- getData.java
+++ getData.java
@@ -1,84 +1,232 @@
+
 import java.io.File;
-import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class getData {
-    public static void main(String[] args) throws IOException {
-        File file = new File("/Users/blagojnikolov/Documents/VSCode/tmpA.txt");
+public class getData extends findMethods {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("/Users/blagojnikolov/Desktop/@tmp/reportDSK.csv");
+
+        List<String[]> resToArr = read(file);
+        // printOneLine(resToArr, 4);
+
+        printDealers(file);
 
         System.out.println();
-        List<List<Integer>> listOfInt = dataToInteger(readFromFile(file));
-        System.out.println(checkIntegers(listOfInt));
-        System.out.println();
+        for (int row = 0; row < resToArr.size(); row++) {
+            if (!getMoneyToStr(resToArr.get(row), "\\d{2,3} \\d{2} BGN")
+                    .equalsIgnoreCase("No such matches")) {
+                System.out.println(
+                        resToArr.get(row)[3].trim() + ": "
+                                + getMoneyToStr(resToArr.get(row), "\\d{2,3} \\d{2} BGN"));
+            }
+        }
+
+        for (Map.Entry<Integer, String> el : findInLine(resToArr, "ЕВН ЕЛ. ЕНЕРГИЯ -ЕЛ.КАНАЛИ").entrySet())
+            System.out.println(el.getKey() + " - " + el.getValue());
+
+        allSpendMoneyA(file);
 
     }
 
-    private static String readFromFile(File file) throws IOException {
-        String data = " ";
+    protected static void printDealers(File file) throws FileNotFoundException {
+        System.out.println();
+        String vivacom = "VIVACOM АБОНАМЕНТЕН ПЛАН";
+        String electrohold = "ЕЛЕКТРОХОЛД ПРОДАЖБИ АД";
+        String evn = "ЕВН ЕЛ. ЕНЕРГИЯ - ЕЛ.КАНАЛИ";
+        String netsurf = "НЕТ - СЪРФ.НЕТ";
+        String chez = "ЧЕЗ";
+        String date = "15.04.2022";
+
+        System.out.print("-----------------------------------");
+        find(file, electrohold);
+        findMoneyFromDealer(file, electrohold);
+        find(file, vivacom);
+        findMoneyFromDealer(file, vivacom);
+        find(file, evn);
+        findMoneyFromDealer(file, evn);
+        find(file, netsurf);
+        findMoneyFromDealer(file, netsurf);
+        System.out.println("-----------------------------------");
+        System.out.printf("All spend sum: %.2fBGN%n", allSpendMoney(file));
+        System.out.println("-----------------------------------");
+
+    }
+
+    protected static void printOneLine(List<String[]> line, int lineNumber) {
+        System.out.println();
+        for (int string = 0; string < line.get(lineNumber).length; string++) {
+            System.out.print(line.get(lineNumber)[string] + " ");
+        }
+        System.out.println();
+    }
+
+    protected static void print(List<String[]> readFromFile, String dealer) {
+        Map<Integer, String> result = findInLine(readFromFile, dealer);
+        for (Map.Entry<Integer, String> el : result.entrySet())
+            System.out.println("row: " + el.getKey() + " - " + el.getValue() + " ");
+    }
+
+    // ! Търси колко пъти "some" се среща във всеки ред(Масив от Str) в листа "line"
+    // * Виждаш какво връща метода */
+    protected static Map<Integer, String> findInLine(List<String[]> line, String some) {
+        some.trim();
+        System.out.println();
+        int lineNumber = -1, cnt;
+
+        Map<Integer, String> result = new HashMap<>();
+        for (int row = 0; row < line.size(); row++) {
+            cnt = 0;
+            for (int string = 0; string < line.get(row).length; string++) {
+                String thisLine = line.get(row)[string].trim();
+                if (some.equalsIgnoreCase(thisLine)) {
+                    lineNumber = row;
+                    result.put(lineNumber, some);
+                    cnt++;
+
+                }
+            }
+        }
+        // System.out.println();
+        return result;
+    }
+
+    // ! Връща List. Всеки един ред от List е масив от Str:
+    protected static List<String[]> read(File file) throws FileNotFoundException {
         @SuppressWarnings("resource")
         Scanner scanner = new Scanner(file);
-        while (scanner.hasNextLine()) {
-            data = scanner.nextLine();
+        List<String[]> readList = new ArrayList<>();
+        List<String> oneLine;
+        String[] line = null;
+
+        while (scanner.hasNext()) {
+            oneLine = new ArrayList<>();
+            line = scanner.nextLine()
+                    .replaceAll("[\"']", "").trim().split(",");
+            readList.add(line);
         }
-        System.out.println(data);
-        return data;
+        return readList;
     }
 
-    private static List<List<Integer>> dataToInteger(String rowData) {
-        List<List<Integer>> listOfInt = new ArrayList<>();
-        List<String> listOfStrings = new ArrayList<>();
+    protected static String findDate(String input) {
+        String dateRgx = "\\d{2}\\.\\d{2}\\.\\d{4}";
 
-        String StrinG = getString(rowData);
-        listOfStrings = getSubString(StrinG);
+        Pattern datePattern = Pattern.compile(dateRgx);
+        Matcher dateMatcher = datePattern.matcher(input);
+        if (dateMatcher.find()) {
+            return dateMatcher.group();
+        } else
+            return "No such matches";
+    }
 
-        for (String el : listOfStrings) {
-            List<Integer> tmp = new ArrayList<>();
-            String[] arr = el.trim().split(", ");
-            for (String elArr : arr) {
-                tmp.add(Integer.parseInt(elArr));
+    // ! Стринга "109 78 BGN" се форматира в "109 78 BGN"
+    protected static String formatMoney(String[] input) {
+        StringBuilder stb = new StringBuilder();
+        stb.append(input[5]);
+        stb.append(" ");
+        stb.append(input[6]);
+        return stb.toString();
+    }
+
+    // ! Намира всяко едно съвпадение на "109 37 BGN" за даден ред.
+    // * Връща "109.37 BGN" */
+    protected static String getMoneyToStr(String[] input, String some) {
+        Pattern datePattern = Pattern.compile(some);
+        // * За всеки един ред от масива търси съвпадението - (109 37 BGN) */
+        for (int i = 0; i < input.length; i++) {
+            Matcher dateMatcher = datePattern.matcher(formatMoney(input));
+            if (dateMatcher.find()) {
+                StringBuilder stb = new StringBuilder();
+                // return dateMatcher.group();
+                stb.append(input[5]);
+                stb.append(".");
+                stb.append(input[6]);
+                return stb.toString();
             }
-            listOfInt.add(tmp);
         }
-        for (List<Integer> el : listOfInt)
-            System.out.println(el); // ! <-
-        return listOfInt;
+        return "No such matches";
     }
 
-    private static String getString(String rowData) {
-        int fromIdx = rowData.indexOf('[');
-        int toIdx = rowData.lastIndexOf(']');
-        String StrinG = rowData.substring(fromIdx + 1, toIdx);
-        return StrinG;
-    }
+    protected static Double allSpendMoney(File file) throws FileNotFoundException {
+        List<String> tmp = new ArrayList<>();
+        List<String[]> input = read(file);
+        double allSum = 0.0;
 
-    private static List<String> getSubString(String rowData) {
-        List<String> listOfStrings = new ArrayList<>();
-        int fromIdx = rowData.indexOf("[");
-        int toIdx = rowData.indexOf("]");
-        int cnt = 0;
-        while (cnt < 3) {
-            String tmpSubString = rowData.substring(fromIdx + 1, toIdx);
-            listOfStrings.add(tmpSubString);
-
-            fromIdx = rowData.indexOf("[", toIdx);
-            toIdx = rowData.indexOf("]", fromIdx);
-            cnt++;
-        }
-        return listOfStrings;
-    }
-
-    private static List<Integer> checkIntegers(List<List<Integer>> list) {
-        List<Integer> sumOfRowsNum = new ArrayList<>();
-        int sum;
-        for (int i = 0; i < list.size(); i++) {
-            sum = 0;
-            for (int j = 0; j < list.get(i).size(); j++) {
-                sum += list.get(i).get(j);
+        for (int i = 0; i < input.size(); i++) {
+            for (int j = 0; j < input.get(j).length; j++) {
+                String money = getMoneyToStr(input.get(i), "\\d{2,3} \\d{2} BGN")
+                        .replaceAll(" BGN", "");
+                if (!money.equalsIgnoreCase("No such matches")) {
+                    allSum += Double.parseDouble(money);
+                    tmp.add(money);
+                    break;
+                }
             }
-            sumOfRowsNum.add(sum);
         }
-        return sumOfRowsNum;
+        System.out.println(tmp);
+        return allSum;
     }
+
+    protected static Double allSpendMoneyA(File file) throws FileNotFoundException {
+        List<String> tmp = new ArrayList<>();
+        List<String[]> input = read(file);
+        double allSum = 0.0;
+        String rgx = ("VIVACOM АБОНАМЕНТЕН ПЛАН | ЕЛЕКТРОХОЛД ПРОДАЖБИ АД | ЕВН ЕЛ. ЕНЕРГИЯ - ЕЛ.КАНАЛИ |  НЕТ - СЪРФ.НЕТ | ЧЕЗ");
+        for (int i = 0; i < input.size(); i++) {
+
+            if (!find(input.get(i), rgx).equalsIgnoreCase("No such matches")) {
+
+                for (int j = 0; j < input.get(j).length; j++) {
+                    String money = getMoneyToStr(input.get(i), "\\d{2,3} \\d{2} BGN")
+                            .replaceAll(" BGN", "");
+                    if (!money.equalsIgnoreCase("No such matches")) {
+                        allSum += Double.parseDouble(money);
+                        tmp.add(money);
+                        break;
+                    }
+                }
+            }
+        }
+        System.out.println(tmp);
+        System.out.println(allSum);
+        return allSum;
+    }
+
+    //// protected static String getEachMoneyToStr(String[] input, String some){
+    //// Pattern datePattern = Pattern.compile(some);
+
+    //// Matcher dateMatcher = datePattern.matcher(formatMoney(input));
+    //// if (dateMatcher.find()) {
+    //// StringBuilder stb = new StringBuilder();
+    //// return dateMatcher.group();
+    //// stb.append(input[5]);
+    //// stb.append(",");
+    //// stb.append(input[6]);
+    //// return stb.toString();
+    //// }
+
+    //// return "No such matches";
+    //// }
+
+    // * protected static Double getMoneyLikeDigit(){}
+
+    // ! Търси "some" само в един ред от масива
+    protected static String find(String[] input, String some) {
+        Pattern datePattern = Pattern.compile(some);
+
+        for (int i = 0; i < input.length; i++) {
+            Matcher dateMatcher = datePattern.matcher(input[i]);
+            if (dateMatcher.find()) {
+                return dateMatcher.group();
+            }
+        }
+        return "No such matches";
+    }
+
 }
